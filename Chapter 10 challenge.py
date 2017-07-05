fc8912f7a7377f50a5d6622cac495ac2ae929fc2
Marc Smith <marc0056@mrh2djg8wl.rackspace.corp>
2017-07-05 11:22:58 -0500

    committing the chapter 10 challenge which was the hangman game

---
+++ Chapter 10 challenge.py
@@ -0,0 +1,35 @@
+#Chapter 10 Challenge (Hangman Game)
+import random
+
+
+def hangman():
+    word_list = ["python", 'java', 'kolaches', 'rackspace']
+    random_number = random.randint(0, 3)
+    word = word_list[random_number]
+    wrong_guesses = 0
+    stages = ["", "________      ", "|      |      ", "|      0      ", "|     /|\     ", "|     / \     ", "|"]
+    remaining_letters = list(word)
+    letter_board = ["__"] * len(word)
+    win = False
+    print('Welcome to Hangman')
+    while wrong_guesses < len(stages) - 1:
+        print('\n')
+        guess = input("Guess a letter")
+        if guess in remaining_letters:
+            character_index = remaining_letters.index(guess)
+            letter_board[character_index] = guess
+            remaining_letters[character_index] = '$'
+        else:
+            wrong_guesses += 1
+        print((' '.join(letter_board)))
+        print('\n'.join(stages[0: wrong_guesses + 1]))
+        if '__' not in letter_board:
+            print('You win! The word was:')
+            print(' '.join(letter_board))
+            win = True
+            break
+    if not win:
+        print('\n'.join(stages[0: wrong_guesses]))
+        print('You lose! The word was {}'.format(word))
+
+hangman()
